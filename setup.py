f5735d6282434fc695e485ed936c90a73a8eb797
Gustavo Ximenez <134465101+ggximenez@users.noreply.github.com>
2024-10-21 00:05:20 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pofium', 
-    version='0.1.0',  
+    version='0.1.1',  
     description='Pacote para download e processamento dos microdados da POF do IBGE.',
     long_description=open('README.md', encoding='utf-8').read(),  # Certifique-se de que o README.md existe
     long_description_content_type='text/markdown',
